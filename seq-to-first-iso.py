3f835bd0cdb1f48fde53e94b3d89cfff93d42697
lilianyc <lilianyangcrosson@gmail.com>
2019-03-18 16:44:20 +0100

    Add CLI parsing with argparse

--- seq-to-first-iso.py
+++ seq-to-first-iso.py
@@ -1,5 +1,6 @@
 
-"""
+"""Create a tsv file from a file of sequences.
+
 Read a file composed of sequences of amino acids on each line and
 return sequence, mass, formula, M0 and M1 in normal and C[12] conditions
 as a tsv file.
@@ -7,14 +8,83 @@ as a tsv file.
 Naming conventions for isotopes follow pyteomics's conventions.
 """
 
-import sys
+__authors__ = "Lilian Yang-crosson"
+__copyright__ = ""
+__credits__ = [""]
+__license__ = ""
+__version__ = "0.1"
+__maintainer__ = ""
+__email__ = ""
+
+import argparse
 from pathlib import Path
 
 import pandas as pd
 from pyteomics import mass
 
 
-USAGE_ERROR = "Usage: python seq-to-first-iso.py filename"
+USAGE_ERROR = "Usage: python seq-to-first-iso.py filename [-o output] [-n aa]"
+# Note: pyteomics also have H- and -OH that can be used for sequences
+# which are not supported in this version.
+AMINO_ACIDS = set("ACDEFGHIKLMNPQRSTVWYUO")
+
+
+def user_input():
+    """
+    Handle the user parameter from the command line.
+
+    Parse the command line parameter and build the list of input files.
+    """
+    parser = argparse.ArgumentParser(
+        description="Read a file of sequences and creates a tsv file")
+
+    # Input file is required as a positional argument.
+    parser.add_argument("input", type=Path, help="file to parse")
+
+    # Optional arguments.
+    parser.add_argument("-o", "--output", nargs="?", type=Path,
+                        help="name of output file")
+    parser.add_argument("-n", "--non_labelled_aa", nargs="+",
+                        metavar="amino_a",
+                        help="amino acids with default abundance")
+
+    options = parser.parse_args()
+    return options
+
+
+def sequence_parser(file):
+    """Return a list of peptide sequences parsed from a file.
+
+    Take a Path() object as argument, return a list of uppercase peptides
+    or exit if the list is empty.
+    """
+    # Check if file exists.
+    if not file.is_file():
+        exit("Error: file {} does not exist in current directory {}\n".format(
+            file, file.cwd()) + USAGE_ERROR)
+
+    # Obtain a list of sequences as string if they are amino acids.
+    with open(file, "r") as filin:
+        sequences = []
+        ignored_lines = 0
+        for sequence in filin:
+            upper_sequence = sequence.upper()
+            # Character not recognized as amino acid.
+            if not set(upper_sequence) - AMINO_ACIDS:
+                print(len(upper_sequence))
+                sequences.append(upper_sequence)
+            else:
+                ignored_lines += 1
+
+    # Check if the file format is correct.
+    if not sequences:
+        exit("Error: incorrect format, make sure that lines "
+             "in {} are valid sequences of amino acids".format(file))
+    if ignored_lines:
+        print("{} lines ignored out of {}".format(ignored_lines,
+                                                  ignored_lines+len(sequences)
+                                                  ))
+    return sequences
 
 
 def compute_M0(f, a):
@@ -75,7 +145,6 @@ def compute_M1(f, a):
 
 
 if __name__ == "__main__":
-
     # Default isotopic abundances from MIDAs website:
     # https://www.ncbi.nlm.nih.gov/CBBresearch/Yu/midas/index.html .
     isotopic_abundance = {"H[1]": 0.999885, "H[2]": 0.000115,
@@ -84,37 +153,26 @@ if __name__ == "__main__":
                           "O[16]": 0.99757, "O[17]": 0.00038, "O[18]": 0.00205,
                           "S[32]": 0.9493,  "S[33]": 0.0076,  "S[34]": 0.0429}
 
-    # Check if usage is correct.
-    if len(sys.argv) < 2:
-        exit("Error: not enough arguments  provided\n" + USAGE_ERROR)
-    elif not Path.is_file(Path(sys.argv[1])):
-        exit("Error: file {} does not exist in current directory {}\n".format(
-                sys.argv[1], str(Path.cwd()))
-             + USAGE_ERROR)
+    options = user_input()
+    input_file = options.input
+    auxotrophic_aa = options.non_labelled_aa
+    print(auxotrophic_aa)
+    sequences = sequence_parser(input_file)
 
-    filename = sys.argv[1]
-
-    # Obtain a list of sequences as string.
-    with open(filename, "r") as filin:
-        sequences = []
-        # Trim the newline caracters.
-        for sequence in filin:
-            sequences.append(sequence.strip())
-
-    # Using dataframes to speed up calculations.
+    # Dataframe of sequences.
     df_peptides = pd.DataFrame({"sequence": sequences})
 
     # Add mass and formulas of sequences
     df_peptides["mass"] = df_peptides["sequence"].map(mass.calculate_mass)
     df_peptides["formula"] = df_peptides["sequence"].map(mass.Composition)
 
-    # M0 and M1 in normal conditions.
+    # Add M0 and M1 in normal conditions.
     df_peptides["M0_NC"] = df_peptides["formula"].apply(compute_M0,
                                                         a=isotopic_abundance)
     df_peptides["M1_NC"] = df_peptides["formula"].apply(compute_M1,
                                                         a=isotopic_abundance)
 
-    # M0 and M1 in 99.99 % of C[12].
+    # Add M0 and M1 in 99.99 % of C[12].
     prop = 0.9999
     isotopic_abundance["C[12]"] = prop
     isotopic_abundance["C[13]"] = 1-prop
@@ -123,9 +181,11 @@ if __name__ == "__main__":
     df_peptides["M1_12C"] = df_peptides["formula"].apply(compute_M1,
                                                          a=isotopic_abundance)
 
-    # Take first 100 rows of df_peptides.
-    sample_size = 100
-    small_df = df_peptides[:sample_size]
+    # Choose output filename.
+    if not options.output:
+        output_file = input_file.stem + ".tsv"
+    else:
+        output_file = options.output
 
     # Import dataframe to tsv file.
-    small_df.to_csv("first-iso.tsv", sep="\t")
+    df_peptides.to_csv(output_file, sep="\t")
