39eda9067da5b881b4c4ce705d8dc4603f4bf789
Pierre Poulain <pierre.poulain@cupnet.net>
2019-12-05 11:03:17 +0100

    Format code for PEP8/257 compliance

--- seq_to_first_iso/seq_to_first_iso.py
+++ seq_to_first_iso/seq_to_first_iso.py
@@ -52,8 +52,10 @@ from pyteomics import mass
 from seq_to_first_iso import __version__
 
 
-USAGE_ERROR = "Usage: python seq-to-first-iso.py filename.tsv sequence_column_name charge_column_name " \
-             + "[-o output] [-u aa]"
+USAGE_ERROR = ("Usage: python seq-to-first-iso.py filename.tsv "
+               "sequence_column_name charge_column_name "
+               "[-o output] [-u aa]"
+               )
 # Note: pyteomics also have U, O, H- and -OH that can be used for sequences
 # which are not supported in this version.
 AMINO_ACIDS = set("ACDEFGHIKLMNPQRSTVWY")
@@ -89,11 +91,11 @@ log.setLevel(logging.INFO)
 # https://www.ncbi.nlm.nih.gov/CBBresearch/Yu/midas/index.html .
 # X is C with default natural abundance.
 natural_abundance = {"H[1]": 0.999885, "H[2]": 0.000115,
-                    "C[12]": 0.9893,  "C[13]": 0.0107,
-                    "X[12]": 0.9893,  "X[13]": 0.0107,
-                    "N[14]": 0.99632, "N[15]": 0.00368,
-                    "O[16]": 0.99757, "O[17]": 0.00038, "O[18]": 0.00205,
-                    "S[32]": 0.9493,  "S[33]": 0.0076,  "S[34]": 0.0429}
+                     "C[12]": 0.9893, "C[13]": 0.0107,
+                     "X[12]": 0.9893, "X[13]": 0.0107,
+                     "N[14]": 0.99632, "N[15]": 0.00368,
+                     "O[16]": 0.99757, "O[17]": 0.00038, "O[18]": 0.00205,
+                     "S[32]": 0.9493, "S[33]": 0.0076, "S[34]": 0.0429}
 
 C12_abundance = dict(natural_abundance)
 prop = 0.9999
@@ -121,12 +123,16 @@ def user_input(args):
 
     """
     parser = argparse.ArgumentParser(
-        description="Read a tsv file with sequences and charges and compute intensity of first isotopologues")
-
+        description=("Read a tsv file with sequences and charges "
+                     "and compute intensity of first isotopologues"
+                     ))
     # Input file is required as a positional argument.
-    parser.add_argument("input_file_name", type=Path, help="file to parse in .tsv format")
-    parser.add_argument("sequence_col_name", type=str, help="column name with sequences")
-    parser.add_argument("charge_col_name", type=str, help="column name with charges")
+    parser.add_argument("input_file_name",
+                        type=Path, help="file to parse in .tsv format")
+    parser.add_argument("sequence_col_name",
+                        type=str, help="column name with sequences")
+    parser.add_argument("charge_col_name",
+                        type=str, help="column name with charges")
 
     # Optional arguments.
     parser.add_argument("-o", "--output", type=str,
@@ -143,9 +149,10 @@ def user_input(args):
     # Check if file exists.
     if not options.input_file_name.is_file():
         log.error(f"file {options.input_file_name} does not exist in "
-                  f"current directory '{options.input_file_name.cwd()}'\n{USAGE_ERROR}"
+                  f"current directory '{options.input_file_name.cwd()}'\n"
+                  f"{USAGE_ERROR}"
                   )
-        exit()
+        sys.exit()
 
     # Check if amino acids are correct.  If not, tell which one.
     if not options.unlabelled_aa:
@@ -155,7 +162,7 @@ def user_input(args):
         options.unlabelled_aa = options.unlabelled_aa.split(",")
         # Convert amino acids to uppercase for compatibility.
         options.unlabelled_aa = [char.upper()
-                                   for char in options.unlabelled_aa]
+                                 for char in options.unlabelled_aa]
         unrecognized_aa = []
 
         for arg in options.unlabelled_aa:
@@ -192,7 +199,7 @@ def parse_input_file(filename, sep="\t"):
         If the input file is not found.
     Exception
         If the input file cannot be read with pandas.
-    
+
     """
     if not sep:
         log.warning("Separator is empty, default value '\t' used.")
@@ -232,7 +239,7 @@ def filter_input_dataframe(df, sequence_col_name, charge_col_name):
     ------
     KeyError
         If the sequence or charge column is not found.
-    
+
     """
     line_count, row_count = df.shape
     log.info(f"Found {line_count} lines and {row_count} columns")
@@ -245,25 +252,27 @@ def filter_input_dataframe(df, sequence_col_name, charge_col_name):
     # Keep only sequences and charges column from original dataframe.
     df = df[[sequence_col_name, charge_col_name]]
     # Rename sequences and charges column to internal naming scheme.
-    df.rename(columns={sequence_col_name: "sequence", charge_col_name: "charge"})
+    df.rename(columns={sequence_col_name: "sequence",
+                       charge_col_name: "charge"})
     return df
 
 
 def check_amino_acids(seq):
-    r""" Check elements of a sequence are known amino acids.
+    r"""Check elements of a sequence are known amino acids.
 
     Parameters
     ----------
     seq : str
         Peptide sequence.
-    
+
     Returns
     -------
     Tuple of two str
-        | (sequence, "") if the sequence is composed 
+        | (sequence, "") if the sequence is composed
         |                of allowed amino acids
-        | ("", "Unrecognized amino acids.") if the sequence is composed 
-        |                                  of unallowed amino acids.
+        | ("", "Unrecognized amino acids.") if the sequence is composed
+        |                                   of unallowed amino acids.
+
     """
     if not(set(seq) - AMINO_ACIDS) and seq:
         return seq, ""
@@ -321,36 +330,37 @@ def compute_M1(f, a):
 
     """
     M1 = (
-          (f["C"] * a["C[12]"]**(f["C"]-1) * a["C[13]"]
-              * a["H[1]"]**f["H"]
-              * a["N[14]"]**f["N"]
-              * a["O[16]"]**f["O"]
-              * a["S[32]"]**f["S"])
+        (f["C"] * a["C[12]"]**(f["C"]-1)
+         * a["C[13]"]
+         * a["H[1]"]**f["H"]
+         * a["N[14]"]**f["N"]
+         * a["O[16]"]**f["O"]
+         * a["S[32]"]**f["S"])
 
-          + (f["H"] * a["C[12]"]**f["C"]
-              * a["H[1]"]**(f["H"]-1) * a["H[2]"]
-              * a["N[14]"]**f["N"]
-              * a["O[16]"]**f["O"]
-              * a["S[32]"]**f["S"])
+        + (f["H"] * a["C[12]"]**f["C"]
+           * a["H[1]"]**(f["H"]-1) * a["H[2]"]
+           * a["N[14]"]**f["N"]
+           * a["O[16]"]**f["O"]
+           * a["S[32]"]**f["S"])
 
-          + (f["N"] * a["C[12]"]**f["C"]
-              * a["H[1]"]**f["H"]
-              * a["N[14]"]**(f["N"]-1) * a["N[15]"]
-              * a["O[16]"]**f["O"]
-              * a["S[32]"]**f["S"])
+        + (f["N"] * a["C[12]"]**f["C"]
+           * a["H[1]"]**f["H"]
+           * a["N[14]"]**(f["N"]-1) * a["N[15]"]
+           * a["O[16]"]**f["O"]
+           * a["S[32]"]**f["S"])
 
-          + (f["O"] * a["C[12]"]**f["C"]
-              * a["H[1]"]**f["H"]
-              * a["N[14]"]**f["N"]
-              * a["O[16]"]**(f["O"]-1) * a["O[17]"]
-              * a["S[32]"]**f["S"])
+        + (f["O"] * a["C[12]"]**f["C"]
+           * a["H[1]"]**f["H"]
+           * a["N[14]"]**f["N"]
+           * a["O[16]"]**(f["O"]-1) * a["O[17]"]
+           * a["S[32]"]**f["S"])
 
-          + (f["S"] * a["C[12]"]**f["C"]
-              * a["H[1]"]**f["H"]
-              * a["N[14]"]**f["N"]
-              * a["O[16]"]**f["O"]
-              * a["S[32]"]**(f["S"]-1) * a["S[33]"])
-          )
+        + (f["S"] * a["C[12]"]**f["C"]
+           * a["H[1]"]**f["H"]
+           * a["N[14]"]**f["N"]
+           * a["O[16]"]**f["O"]
+           * a["S[32]"]**(f["S"]-1) * a["S[33]"])
+    )
     return M1
 
 
@@ -432,49 +442,44 @@ def compute_M1_nl(f, a):
 
     """
     M1 = (
-          (f["C"] * a["C[12]"]**(f["C"]-1) * a["C[13]"]
-              * a["X[12]"]**f["X"]
-              * a["H[1]"]**f["H"]
-              * a["N[14]"]**f["N"]
-              * a["O[16]"]**f["O"]
-              * a["S[32]"]**f["S"])
-
-          + (f["X"] * a["C[12]"]**f["C"]
-              * a["X[12]"]**(f["X"]-1) * a["X[13]"]
-              * a["H[1]"]**f["H"]
-              * a["N[14]"]**f["N"]
-              * a["O[16]"]**f["O"]
-              * a["S[32]"]**f["S"])
-
-          + (f["H"] * a["C[12]"]**f["C"]
-              * a["X[12]"]**f["X"]
-              * a["H[1]"]**(f["H"]-1) * a["H[2]"]
-              * a["N[14]"]**f["N"]
-              * a["O[16]"]**f["O"]
-              * a["S[32]"]**f["S"])
-
-          + (f["N"] * a["C[12]"]**f["C"]
-              * a["X[12]"]**f["X"]
-              * a["H[1]"]**f["H"]
-              * a["N[14]"]**(f["N"]-1) * a["N[15]"]
-              * a["O[16]"]**f["O"]
-              * a["S[32]"]**f["S"])
-
-          + (f["O"] * a["C[12]"]**f["C"]
-              * a["X[12]"]**f["X"]
-              * a["H[1]"]**f["H"]
-              * a["N[14]"]**f["N"]
-              * a["O[16]"]**(f["O"]-1) * a["O[17]"]
-              * a["S[32]"]**f["S"])
-
-          + (f["S"] * a["C[12]"]**f["C"]
-              * a["X[12]"]**f["X"]
-              * a["H[1]"]**f["H"]
-              * a["N[14]"]**f["N"]
-              * a["O[16]"]**f["O"]
-              * a["S[32]"]**(f["S"]-1) * a["S[33]"])
-          )
-
+        (f["C"] * a["C[12]"]**(f["C"]-1)
+         * a["C[13]"]
+         * a["X[12]"]**f["X"]
+         * a["H[1]"]**f["H"]
+         * a["N[14]"]**f["N"]
+         * a["O[16]"]**f["O"]
+         * a["S[32]"]**f["S"])
+        + (f["X"] * a["C[12]"]**f["C"]
+           * a["X[12]"]**(f["X"]-1) * a["X[13]"]
+           * a["H[1]"]**f["H"]
+           * a["N[14]"]**f["N"]
+           * a["O[16]"]**f["O"]
+           * a["S[32]"]**f["S"])
+        + (f["H"] * a["C[12]"]**f["C"]
+           * a["X[12]"]**f["X"]
+           * a["H[1]"]**(f["H"]-1) * a["H[2]"]
+           * a["N[14]"]**f["N"]
+           * a["O[16]"]**f["O"]
+           * a["S[32]"]**f["S"])
+        + (f["N"] * a["C[12]"]**f["C"]
+           * a["X[12]"]**f["X"]
+           * a["H[1]"]**f["H"]
+           * a["N[14]"]**(f["N"]-1) * a["N[15]"]
+           * a["O[16]"]**f["O"]
+           * a["S[32]"]**f["S"])
+        + (f["O"] * a["C[12]"]**f["C"]
+           * a["X[12]"]**f["X"]
+           * a["H[1]"]**f["H"]
+           * a["N[14]"]**f["N"]
+           * a["O[16]"]**(f["O"]-1) * a["O[17]"]
+           * a["S[32]"]**f["S"])
+        + (f["S"] * a["C[12]"]**f["C"]
+           * a["X[12]"]**f["X"]
+           * a["H[1]"]**f["H"]
+           * a["N[14]"]**f["N"]
+           * a["O[16]"]**f["O"]
+           * a["S[32]"]**(f["S"]-1) * a["S[33]"])
+    )
     return M1
 
 
@@ -532,11 +537,12 @@ def get_charge_composition(charge):
     ----------
     charge : int
         Peptide charge.
-    
+
     Returns
     -------
     pyteomics.mass.Composition
         Composition of the change (H+).
+
     """
     charge_composition = mass.Composition()
     charge_composition["H"] = charge
@@ -588,10 +594,10 @@ def compute_intensities(df_peptides, unlabelled_aa):
     -------
     pandas.DataFrame
         | Dataframe with :
-        |                  sequence, charge, 
+        |                  sequence, charge,
         |                  stfi_neutral_mass, stfi_formula, stfi_formula_X,
         |                  stfi_ M0_NC, stfi_M1_NC, stfi_M0_12C, stfi_M1_12C.
-    
+
     Notes
     -----
     | Supports Xtandem's Post-Translational Modification notation (0.4.0).
@@ -599,51 +605,69 @@ def compute_intensities(df_peptides, unlabelled_aa):
     """
     log.info("Reading sequences.")
     # Remove potential HTML residues from sequences.
-    df_peptides["sequence_clean"] = df_peptides["sequence"].str.replace("&gt;", ">", case = False)
-    
-    # Extract modifications 
-    df_peptides["modification"] = df_peptides["sequence_clean"].str.findall(XTANDEM_MOD_PATTERN)
-     
+    df_peptides["sequence_clean"] = (
+        df_peptides["sequence"].str.replace("&gt;", ">", case=False)
+    )
+
+    # Extract modifications.
+    df_peptides["modification"] = (
+        df_peptides["sequence_clean"].str.findall(XTANDEM_MOD_PATTERN)
+    )
+
     # Remove modifications and capitalize sequence.
-    df_peptides["sequence_without_mod"] = df_peptides["sequence_clean"].str.replace(XTANDEM_MOD_PATTERN, "").str.upper()
+    df_peptides["sequence_without_mod"] = (
+        df_peptides["sequence_clean"]
+        .str.replace(XTANDEM_MOD_PATTERN, "")
+        .str.upper()
+    )
 
     # Check that sequences without modifications are real peptide sequences.
-    df_peptides["sequence_to_process"], df_peptides["log"] = zip(*df_peptides["sequence_without_mod"].apply(check_amino_acids))
+    df_peptides["sequence_to_process"], df_peptides["log"] = (
+        zip(*df_peptides["sequence_without_mod"].apply(check_amino_acids))
+    )
 
     # Split labelled and unlabelled amino acids from peptide sequences.
     df_peptides["sequence_labelled"], df_peptides["sequence_unlabelled"] = zip(
-            *df_peptides["sequence_to_process"].apply(separate_labelled,
-                                                      unlabelled_aa=unlabelled_aa))
+        *df_peptides["sequence_to_process"]
+        .apply(separate_labelled, unlabelled_aa=unlabelled_aa)
+    )
 
     log.info("Computing composition and formula.")
     # Get composition from modifications
-    df_peptides["composition_mod"] = df_peptides["modification"].apply(get_mods_composition)
+    df_peptides["composition_mod"] = (
+        df_peptides["modification"].apply(get_mods_composition)
+    )
 
     # Get Composition from labelled peptide sequence.
-    df_peptides["composition_labelled"] = df_peptides["sequence_labelled"].apply(mass.Composition)
+    df_peptides["composition_labelled"] = (
+        df_peptides["sequence_labelled"].apply(mass.Composition)
+    )
 
     # Get Composition from unlabelled peptide sequence.
-    # Unlabelled amino acids are not a real peptide sequence, 
+    # Unlabelled amino acids are not a real peptide sequence,
     # hence the 'parsed_sequence' parameter.
     # See https://pyteomics.readthedocs.io/en/latest/api/mass.html
-    df_peptides["composition_unlabelled"] = df_peptides["sequence_unlabelled"].apply(lambda x : mass.Composition(parsed_sequence=x))
+    df_peptides["composition_unlabelled"] = (
+        df_peptides["sequence_unlabelled"]
+        .apply(lambda x: mass.Composition(parsed_sequence=x))
+    )
 
-    # Compute peptide composition
+    # Compute peptide composition.
     df_peptides["composition_peptide_neutral"] = (
-          df_peptides["composition_labelled"] 
+        df_peptides["composition_labelled"]
         + df_peptides["composition_unlabelled"]
         + df_peptides["composition_mod"]
     )
 
     df_peptides["composition_peptide_with_charge"] = (
-          df_peptides["composition_peptide_neutral"]
+        df_peptides["composition_peptide_neutral"]
         + df_peptides["charge"].apply(get_charge_composition)
     )
 
     # Compute peptide composition with X.
     # Carbon atoms from unlabelled peptide are replaced by element X.
     df_peptides["composition_peptide_with_charge_X"] = (
-          df_peptides["composition_labelled"]
+        df_peptides["composition_labelled"]
         + df_peptides["composition_unlabelled"].apply(convert_atom_C_to_X)
         + df_peptides["composition_mod"]
         + df_peptides["charge"].apply(get_charge_composition)
@@ -667,24 +691,20 @@ def compute_intensities(df_peptides, unlabelled_aa):
     log.info("Computing M0 and M1")
     # Can use compute_M0_nl with isotopic abundance twice
     df_peptides["M0_NC"] = (
-        df_peptides["composition_peptide_with_charge_X"].apply(
-                                                        compute_M0_nl,
-                                                        a=natural_abundance)
+        df_peptides["composition_peptide_with_charge_X"]
+        .apply(compute_M0_nl, a=natural_abundance)
     )
     df_peptides["M1_NC"] = (
-        df_peptides["composition_peptide_with_charge_X"].apply(
-                                                        compute_M1_nl,
-                                                        a=natural_abundance)
-    )    
+        df_peptides["composition_peptide_with_charge_X"]
+        .apply(compute_M1_nl, a=natural_abundance)
+    )
     df_peptides["M0_12C"] = (
-        df_peptides["composition_peptide_with_charge_X"].apply(
-                                                        compute_M0_nl,
-                                                        a=C12_abundance)
+        df_peptides["composition_peptide_with_charge_X"]
+        .apply(compute_M0_nl, a=C12_abundance)
     )
     df_peptides["M1_12C"] = (
-        df_peptides["composition_peptide_with_charge_X"].apply(
-                                                        compute_M1_nl,
-                                                        a=C12_abundance)
+        df_peptides["composition_peptide_with_charge_X"]
+        .apply(compute_M1_nl, a=C12_abundance)
     )
 
     return df_peptides.add_prefix('stfi_')
@@ -720,7 +740,7 @@ def cli(args=None):
     df_raw = parse_input_file(options.input_file_name)
     df_filtered = filter_input_dataframe(df_raw,
                                          options.sequence_col_name,
-                                         options.charge_col_name )
+                                         options.charge_col_name)
     df = compute_intensities(df_filtered, options.unlabelled_aa)
 
     # Choose output filename.
@@ -729,15 +749,16 @@ def cli(args=None):
     else:
         output_file = options.output + ".tsv"
 
-    column_of_interest = ["stfi_neutral_mass", 
+    column_of_interest = ["stfi_neutral_mass",
                           "stfi_formula", "stfi_formula_X",
-                          "stfi_M0_NC", "stfi_M1_NC", 
+                          "stfi_M0_NC", "stfi_M1_NC",
                           "stfi_M0_12C", "stfi_M1_12C"]
-    
+
     # Read original file and append STFI data.
     df_old = pd.read_csv(options.input_file_name, sep="\t")
     df_new = pd.concat([df_old, df[column_of_interest]], axis=1)
     df_new.to_csv(output_file, sep="\t", index=False)
+
 
 if __name__ == "__main__":
     cli()  # pragma: no cover
